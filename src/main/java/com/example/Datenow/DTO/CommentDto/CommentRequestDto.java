b99004b585a76bc956f048b9374c9f8bd67f8443
yardyard <yardyard@likelion.org>
2023-01-04 00:27:47 +0900

    fix : Comment 생성 오류 해결

--- src/main/java/com/example/Datenow/DTO/CommentDto/CommentRequestDto.java
+++ src/main/java/com/example/Datenow/DTO/CommentDto/CommentRequestDto.java
@@ -1,25 +1,18 @@
 package com.example.Datenow.DTO.CommentDto;
 
-import com.example.Datenow.domain.Comment;
-import com.example.Datenow.domain.Post.Post;
-import com.example.Datenow.domain.User;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
 
 @Getter
 @Builder
 public class CommentRequestDto {
-
     private String content;
 
-    public Comment toEntity() {
+    // 왜 이걸 지우니까 잘 동작하지???? -> 아마 Comment Entity의 Builder는 content, post, user를 입력받지만 여기 엔티티는 content만 입력받기 때문이지 않을까??
+    /*public Comment toEntity() {
         return Comment.builder()
                 .content(content)
                 .build();
-    }
+    }*/
 }
