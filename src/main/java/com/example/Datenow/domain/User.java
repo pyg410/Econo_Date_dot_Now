270ab00d53731baf2d47731e8cbfa0408230fdc6
park <pyg410@naver.com>
2023-01-07 14:05:20 +0900

    add: user entity

--- src/main/java/com/example/Datenow/domain/User.java
+++ src/main/java/com/example/Datenow/domain/User.java
@@ -1,4 +1,33 @@
 package com.example.Datenow.domain;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String password;
+    private String email;
+    private String nickname;
+    private LocalDate birth;
+    private Long phone_num;
+    private String profile_img;
+    private Boolean gender;
+    private LocalDateTime created_at;
+    private LocalDateTime modified_at;
+
+
 }
